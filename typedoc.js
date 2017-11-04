ba3e6e1c5ab03a78376f3f7289ba982c4650252a
Kyle Machulis <kyle@nonpolynomial.com>
2017-11-03 17:57:39 -0700

    doc: Add typedoc configuration and build steps

---
+++ typedoc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: "file",
+  module: "umd",
+  target: "ES6",
+  exclude: "**/+(test|example|node_modules)/**/*.ts",
+  excludeExternals: true,
+  excludePrivate: true
+}
+
