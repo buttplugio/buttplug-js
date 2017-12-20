e1a6a12deb76d43f93fbca2d8690eb652defa890
Kyle Machulis <kyle@nonpolynomial.com>
2017-12-19 16:39:26 -0800

    feat: Update bluetooth devices to define feature specifications

--- src/server/bluetooth/ButtplugBluetoothDevice.ts
+++ src/server/bluetooth/ButtplugBluetoothDevice.ts
@@ -1,7 +1,7 @@
 import { ButtplugDevice } from "../ButtplugDevice";
 import { IBluetoothDeviceImpl } from "./IBluetoothDeviceImpl";
 
-export class ButtplugBluetoothDevice extends ButtplugDevice {
+export abstract class ButtplugBluetoothDevice extends ButtplugDevice {
   public constructor(aName: string, protected _deviceImpl: IBluetoothDeviceImpl) {
     super(aName);
   }
