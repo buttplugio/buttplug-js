cc7c20744a0030519803a5914d1a3aec12fc9708
Kyle Machulis <kyle@nonpolynomial.com>
2018-01-15 23:58:44 -0800

    feat: Add TestDeviceManager HTML panel
    Shows test devices

--- src/devtools/TestDeviceManager.ts
+++ src/devtools/TestDeviceManager.ts
@@ -1,15 +1,46 @@
 import { EventEmitter } from "events";
-import { IDeviceSubtypeManager } from "../index";
+import { IDeviceSubtypeManager } from "../server/IDeviceSubtypeManager";
 import { TestDevice } from "./TestDevice";
 
 export class TestDeviceManager extends EventEmitter implements IDeviceSubtypeManager {
 
+  public static Get(): TestDeviceManager {
+    if (!TestDeviceManager._testManager) {
+      TestDeviceManager._testManager = new TestDeviceManager();
+    }
+    return TestDeviceManager._testManager;
+  }
+
+  private static _testManager: TestDeviceManager | null = null;
+
   private _isScanning = false;
+  private _testVibrationDevice = new TestDevice("Test Vibration Device", true, false);
+  private _testLinearDevice = new TestDevice("Test Linear Device", false, true);
+
+  protected constructor() {
+    super();
+    console.log("Creating new test device manager!");
+  }
 
   public StartScanning(): void {
     this._isScanning = true;
-    setTimeout(() => this.emit("deviceadded", new TestDevice("Test Device 1")), 250);
-    setTimeout(() => this.StopScanning(), 500);
+    if (!this._testVibrationDevice.Connected) {
+      this._testVibrationDevice.Connected = true;
+      setTimeout(() => this.emit("deviceadded", this._testVibrationDevice), 50);
+    }
+    if (!this._testLinearDevice.Connected) {
+      this._testLinearDevice.Connected = true;
+      setTimeout(() => this.emit("deviceadded", this._testLinearDevice), 50);
+    }
+    setTimeout(() => this.StopScanning(), 100);
+  }
+
+  public get VibrationDevice() {
+    return this._testVibrationDevice;
+  }
+
+  public get LinearDevice() {
+    return this._testLinearDevice;
   }
 
   public StopScanning(): void {
@@ -20,4 +51,5 @@ export class TestDeviceManager extends EventEmitter implements IDeviceSubtypeMan
   public get IsScanning(): boolean {
     return this._isScanning;
   }
+
 }
--- src/devtools/TestDeviceManagerPanel.ts
+++ src/devtools/TestDeviceManagerPanel.ts
@@ -3,6 +3,39 @@ import { TestDeviceManager } from "./TestDeviceManager";
 const jsPanel = require("./jspanel.js");
 require("jspanel4/dist/jspanel.css");
 require("jspanel4/dist/fonts/jsglyph.eot");
+const testPanelHTML = require("./TestDeviceManagerPanel.html").toString();
+require("./TestDeviceManagerPanel.css");
 
 export class TestDeviceManagerPanel {
+  public static ShowTestDeviceManagerPanel() {
+    jsPanel.jsPanel.create({
+      theme:       "primary",
+      headerTitle: "Test Device Manager",
+      position:    "center-top 0 80",
+      contentSize: "350 250",
+      callback: function() {
+        this.content.innerHTML = testPanelHTML;
+        TestDeviceManagerPanel._panel = new TestDeviceManagerPanel();
+      },
+    });
+  }
+
+  protected static _panel: TestDeviceManagerPanel;
+  private _testManager = TestDeviceManager.Get();
+
+  constructor() {
+    document.getElementById("vibratedisconnect")!.addEventListener("click", () => {
+      this._testManager.VibrationDevice.Disconnect();
+    });
+    document.getElementById("lineardisconnect")!.addEventListener("click", () => {
+      this._testManager.LinearDevice.Disconnect();
+    });
+    this._testManager.VibrationDevice.addListener("vibrate", (speed) => {
+      document.getElementById("vibrationspeed")!.innerHTML = speed;
+    });
+    this._testManager.LinearDevice.addListener("linear", (linearobj: any) => {
+      document.getElementById("linearposition")!.innerHTML = linearobj.position;
+      document.getElementById("linearspeed")!.innerHTML = linearobj.speed;
+    });
+  }
 }
