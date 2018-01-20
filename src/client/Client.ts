8b57dd95c5b33f1cd273f3bcf5cc61c7285063c4
Kyle Machulis <kyle@nonpolynomial.com>
2018-01-19 23:06:43 -0800

    chore: Turn device array for client into a getter

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -56,7 +56,7 @@ export class ButtplugClient extends EventEmitter {
     this._connector!.Disconnect();
   }
 
-  public getDevices(): Device[] {
+  public get Devices(): Device[] {
     // While this function doesn't actually send a message, if we don't have a
     // connector, we shouldn't have devices.
     this.CheckConnector();
@@ -91,7 +91,7 @@ export class ButtplugClient extends EventEmitter {
     this.CheckConnector();
     const dev = this._devices.get(aDevice.Index);
     if (dev === undefined) {
-      this._logger.Error(`Device ${aDevice} not available.`);
+      this._logger.Error(`Device ${aDevice.Index} not available.`);
       return Promise.reject(new Error("Device not available."));
     }
     if (dev.AllowedMessages.indexOf(aDeviceMsg.Type) === -1) {
