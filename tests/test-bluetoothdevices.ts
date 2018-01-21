7cae83ad60018788e7e411dc733cc8d9c3162b7e
Kyle Machulis <kyle@nonpolynomial.com>
2018-01-20 22:29:30 -0800

    test: Add basic Launch and Vorze command conversion tests

--- tests/test-bluetoothdevices.ts
+++ tests/test-bluetoothdevices.ts
@@ -2,7 +2,9 @@ import { WebBluetoothMock, DeviceMock, CharacteristicMock, PrimaryServiceMock, G
 import { ButtplugLogger, ButtplugLogLevel } from "../src/core/Logging";
 import { ButtplugClient } from "../src/client/Client";
 import { BPTestClient, SetupTestSuite, WebBluetoothMockObject, MakeMockWebBluetoothDevice } from "./utils";
-import { VibrateCmd, SpeedSubcommand, DeviceInfo, BluetoothDeviceInfo, SingleMotorVibrateCmd } from "../src/index";
+import { VibrateCmd, RotateCmd, SpeedSubcommand, LinearCmd, VectorSubcommand, FleshlightLaunchFW12Cmd,
+         DeviceInfo, BluetoothDeviceInfo, SingleMotorVibrateCmd, RotateSubcommand,
+         VorzeA10CycloneCmd } from "../src/index";
 import { LovenseRev5 } from "../src/server/bluetooth/devices/Lovense";
 import { WeVibe } from "../src/server/bluetooth/devices/WeVibe";
 import { FleshlightLaunch } from "../src/server/bluetooth/devices/FleshlightLaunch";
@@ -53,7 +55,7 @@ describe("WebBluetooth library tests", () => {
     await bp.StopScanning();
     jest.spyOn(mockBT.txChar, "writeValue");
     await expect(bp.SendDeviceMessage(bp.Devices[0], new VibrateCmd([new SpeedSubcommand(0, 1),
-                                                                     new SpeedSubcommand(0, 2)]))).rejects.toThrow();
+                                                                     new SpeedSubcommand(1, 1)]))).rejects.toThrow();
     await bp.SendDeviceMessage(bp.Devices[0], new VibrateCmd([new SpeedSubcommand(0, 1)]));
     expect(mockBT.txChar.writeValue).toBeCalledWith(new Uint8Array([0x0f, 0x03, 0x00, 0xff, 0x00, 0x03, 0x00, 0x00]));
     await bp.SendDeviceMessage(bp.Devices[0], new SingleMotorVibrateCmd(.5));
@@ -67,6 +69,14 @@ describe("WebBluetooth library tests", () => {
     await bp.StartScanning();
     await bp.StopScanning();
     jest.spyOn(mockBT.txChar, "writeValue");
+    await expect(bp.SendDeviceMessage(bp.Devices[0],
+                                      new LinearCmd([new VectorSubcommand(0, 1, 1),
+                                                     new VectorSubcommand(1, 1, 1)]))).rejects.toThrow();
+    await bp.SendDeviceMessage(bp.Devices[0], new FleshlightLaunchFW12Cmd(99, 99));
+    expect(mockBT.txChar.writeValue).toBeCalledWith(new Uint8Array([99, 99]));
+    // TODO: Actual LinearCmd test
+    await bp.StopAllDevices();
+    expect(mockBT.txChar.writeValue).toBeCalledWith(new Uint8Array([0, 0]));
   });
 
   it("should convert vorze commands properly", async () => {
@@ -74,5 +84,14 @@ describe("WebBluetooth library tests", () => {
     await bp.StartScanning();
     await bp.StopScanning();
     jest.spyOn(mockBT.txChar, "writeValue");
+    await expect(bp.SendDeviceMessage(bp.Devices[0],
+                                      new RotateCmd([new RotateSubcommand(0, 1, true),
+                                                     new RotateSubcommand(1, 1, false)]))).rejects.toThrow();
+    await bp.SendDeviceMessage(bp.Devices[0], new RotateCmd([new RotateSubcommand(0, 1, true)]));
+    expect(mockBT.txChar.writeValue).toBeCalledWith(new Uint8Array([0x01, 0x01, (100 | (0x80)) & 0xff]));
+    await bp.SendDeviceMessage(bp.Devices[0], new VorzeA10CycloneCmd(50, false));
+    expect(mockBT.txChar.writeValue).toBeCalledWith(new Uint8Array([0x01, 0x01, 50]));
+    await bp.StopAllDevices();
+    expect(mockBT.txChar.writeValue).toBeCalledWith(new Uint8Array([0x01, 0x01, 0]));
   });
 });
