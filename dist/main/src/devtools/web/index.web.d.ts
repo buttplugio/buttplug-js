c6eea9e8048ac4c0e8d12889711f8d566137d12b
Kyle Machulis <kyle@nonpolynomial.com>
2018-01-29 11:52:46 -0800

    build: Release artifacts for 0.5.3 release

--- dist/main/src/devtools/web/index.web.d.ts
+++ dist/main/src/devtools/web/index.web.d.ts
@@ -1,4 +1,6 @@
-export * from "../index";
+export * from "../TestDevice";
+export * from "../TestDeviceManager";
+export * from "../utils";
 export * from "./LogPanel";
 export * from "./TestDeviceManagerPanel";
 export * from "./utils.web";
--- dist/main/src/devtools/web/index.web.js
+++ dist/main/src/devtools/web/index.web.js
@@ -3,7 +3,9 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("../index"));
+__export(require("../TestDevice"));
+__export(require("../TestDeviceManager"));
+__export(require("../utils"));
 __export(require("./LogPanel"));
 __export(require("./TestDeviceManagerPanel"));
 __export(require("./utils.web"));
