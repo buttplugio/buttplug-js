0dbf76c29445ae2d3d7ff8186f340b924bea52ad
Kyle Machulis <kyle@nonpolynomial.com>
2018-02-04 21:11:47 -0800

    feat: Basic MaxPro support
    Starts vibrating. Has problems stopping.

---
+++ src/server/bluetooth/devices/Maxpro.ts
@@ -0,0 +1,60 @@
+
+import { BluetoothDeviceInfo } from "../BluetoothDeviceInfo";
+import { ButtplugBluetoothDevice } from "../ButtplugBluetoothDevice";
+import { IBluetoothDeviceImpl } from "../IBluetoothDeviceImpl";
+import * as Messages from "../../../core/Messages";
+
+export class Maxpro extends ButtplugBluetoothDevice {
+  public static readonly DeviceInfo = new BluetoothDeviceInfo(["M2"],
+                                                              ["6e400001-b5a3-f393-e0a9-e50e24dcca9e"],
+                                                              { tx: "6e400002-b5a3-f393-e0a9-e50e24dcca9e",
+                                                                rx: "6e400003-b5a3-f393-e0a9-e50e24dcca9e"},
+                                                              Maxpro.CreateInstance);
+
+  public static async CreateInstance(aDeviceImpl: IBluetoothDeviceImpl): Promise<ButtplugBluetoothDevice> {
+    return new Maxpro(aDeviceImpl);
+  }
+
+  public constructor(aDeviceImpl: IBluetoothDeviceImpl) {
+    super(`Maxpro ${aDeviceImpl.Name}` , aDeviceImpl);
+    this.MsgFuncs.set(Messages.StopDeviceCmd.name, this.HandleStopDeviceCmd);
+    this.MsgFuncs.set(Messages.SingleMotorVibrateCmd.name, this.HandleSingleMotorVibrateCmd);
+    this.MsgFuncs.set(Messages.VibrateCmd.name, this.HandleVibrateCmd);
+  }
+
+  public get MessageSpecifications(): object {
+    return {
+      VibrateCmd: { FeatureCount: 1 },
+      SingleMotorVibrateCmd: {},
+      StopDeviceCmd: {},
+    };
+  }
+
+  private HandleVibrateCmd = async (aMsg: Messages.VibrateCmd): Promise<Messages.ButtplugMessage> => {
+    if (aMsg.Speeds.length !== 1) {
+      return new Messages.Error(`Maxpro devices require VibrateCmd to have 1 speed command, ` +
+                                `${aMsg.Speeds.length} sent.`,
+                                Messages.ErrorClass.ERROR_DEVICE,
+                                aMsg.Id);
+    }
+    return await this.HandleSingleMotorVibrateCmd(new Messages.SingleMotorVibrateCmd(aMsg.Speeds[0].Speed,
+                                                                                     aMsg.DeviceIndex,
+                                                                                     aMsg.Id));
+  }
+
+  private HandleStopDeviceCmd = async (aMsg: Messages.StopDeviceCmd): Promise<Messages.ButtplugMessage> => {
+    return await this.HandleSingleMotorVibrateCmd(new Messages.SingleMotorVibrateCmd(0, aMsg.DeviceIndex, aMsg.Id));
+  }
+
+  private HandleSingleMotorVibrateCmd =
+    async (aMsg: Messages.SingleMotorVibrateCmd): Promise<Messages.ButtplugMessage> => {
+      // Speed range for Maxpro toys are 10-100 for some reason.
+      const speed = Math.floor(aMsg.Speed * 100);
+      const data: Uint8Array = new Uint8Array([0x55, 0x04, 0x07, 0xff, 0xff, 0x3f, speed, 0x5f, speed, 0x00]);
+      console.log(data);
+      const checksum = data.reduce((prev, cur) => prev + cur) & 0xFF;
+      data[9] = checksum;
+      await this._deviceImpl.WriteValue("tx", data);
+      return new Messages.Ok(aMsg.Id);
+    }
+}
