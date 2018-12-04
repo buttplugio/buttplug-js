ef9e36cd636b2af9a93694ed7eebcfb3fa540f6e
Kyle Machulis <kyle@nonpolynomial.com>
2018-12-03 23:01:30 -0800

    test: Update devtools TestDeviceManager with new logger requirement

--- src/devtools/TestDeviceManager.ts
+++ src/devtools/TestDeviceManager.ts
@@ -5,6 +5,7 @@ import { ButtplugLogger } from "../index";
 
 export class TestDeviceManager extends EventEmitter implements IDeviceSubtypeManager {
 
+  private _logger: ButtplugLogger = ButtplugLogger.Logger;
   private _isScanning = false;
   private _testVibrationDevice = new TestDevice("Test Vibration Device", true, false, false);
   private _testLinearDevice = new TestDevice("Test Linear Device", false, true, false);
@@ -14,26 +15,30 @@ export class TestDeviceManager extends EventEmitter implements IDeviceSubtypeMan
     super();
   }
 
+  public SetLogger(aLogger: ButtplugLogger) {
+    this._logger = aLogger;
+  }
+
   public ConnectVibrationDevice() {
-    ButtplugLogger.Logger.Debug("TestDeviceManager: Connecting Vibration Device");
+    this._logger.Debug("TestDeviceManager: Connecting Vibration Device");
     this._testVibrationDevice.Connected = true;
     this.emit("deviceadded", this._testVibrationDevice);
   }
 
   public ConnectLinearDevice() {
-    ButtplugLogger.Logger.Debug("TestDeviceManager: Connecting Linear Device");
+    this._logger.Debug("TestDeviceManager: Connecting Linear Device");
     this._testLinearDevice.Connected = true;
     this.emit("deviceadded", this._testLinearDevice);
   }
 
   public ConnectRotationDevice() {
-    ButtplugLogger.Logger.Debug("TestDeviceManager: Connecting Rotation Device");
+    this._logger.Debug("TestDeviceManager: Connecting Rotation Device");
     this._testRotationDevice.Connected = true;
     this.emit("deviceadded", this._testRotationDevice);
   }
 
   public StartScanning(): void {
-    ButtplugLogger.Logger.Debug("TestDeviceManager: Starting Scan");
+    this._logger.Debug("TestDeviceManager: Starting Scan");
     this._isScanning = true;
     // Always emit devices. If they're duplicates, the device manager will weed
     // them out.
@@ -58,7 +63,7 @@ export class TestDeviceManager extends EventEmitter implements IDeviceSubtypeMan
   }
 
   public StopScanning(): void {
-    ButtplugLogger.Logger.Debug("TestDeviceManager: Stopping Scan");
+    this._logger.Debug("TestDeviceManager: Stopping Scan");
     this._isScanning = false;
     this.emit("scanningfinished");
   }
