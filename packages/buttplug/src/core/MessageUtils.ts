8bd0002a21241e01bcb0ab48a8ebf7e676324fd7
Kyle Machulis <kyle@nonpolynomial.com>
2020-03-25 21:09:00 -0700

    chore: Remove json checking for now
    I really think we only need JSON schema checking on the server side,
    and buttplug-js should almost ever be used for the server side with
    encoded data, especially once rust is ready to go. For now, remove
    validation. See if we regret this later.

--- packages/buttplug/src/core/MessageUtils.ts
+++ packages/buttplug/src/core/MessageUtils.ts
@@ -8,33 +8,38 @@
 
 "use strict";
 import {plainToClass} from "class-transformer";
-import * as ajv from "ajv";
+// import * as ajv from "ajv";
 import * as Messages from "./Messages";
 import { ButtplugMessageException } from "./Exceptions";
 import * as buttplugSchema from "../../dependencies/buttplug-schema/schema/buttplug-schema.json";
 
 // Since we're still using the draft 06 schema, we now have to specifically add
 // it to ajv, which defaults to 7.
-const validator = new ajv();
-validator.addMetaSchema(require("ajv/lib/refs/json-schema-draft-06.json"));
-const jsonValidator = validator.compile(buttplugSchema);
+// const validator = new ajv();
+// validator.addMetaSchema(require("ajv/lib/refs/json-schema-draft-06.json"));
+// const jsonValidator = validator.compile(buttplugSchema);
 
 export function CheckMessage(aMsgObj: Messages.ButtplugMessage) {
+  /*
   if (jsonValidator([aMsgObj.toProtocolFormat()])) {
     return;
   }
   // Relay validator errors as an error message locally.
   const errorString = jsonValidator.errors!.map((error) => error.message).join("; ");
   throw new ButtplugMessageException(errorString);
+  */
+ return true;
 }
 
 export function FromJSON(str): Messages.ButtplugMessage[] {
   const msgarray: object[] = JSON.parse(str);
+  /*
   if (!jsonValidator(msgarray)) {
     // Relay validator errors as an error message locally.
     const errorString = jsonValidator.errors!.map((error) => error.message).join("; ");
     throw new ButtplugMessageException(errorString);
   }
+  */
   const msgs: Messages.ButtplugMessage[] = [];
   for (const x of Array.from(msgarray)) {
     // Can't get this to resolve nicely as a type, so just start from any and cast
