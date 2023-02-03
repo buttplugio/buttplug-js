a6c4f355ce71969aa58126a6b56d6aa39c8da463
Pat <pat@uncensorpat.ch>
2023-02-03 18:54:21 +1100

    feat: Add Node Websocket Client Connector
    Allows the package to be used from Node environments as well as browsers

---
+++ src/client/ButtplugNodeWebsocketClientConnector.ts
@@ -0,0 +1,19 @@
+/*!
+ * Buttplug JS Source Code File - Visit https://buttplug.io for more info about
+ * the project. Licensed under the BSD 3-Clause license. See LICENSE file in the
+ * project root for full license information.
+ *
+ * @copyright Copyright (c) Nonpolynomial Labs LLC. All rights reserved.
+ */
+
+'use strict';
+
+import { ButtplugBrowserWebsocketClientConnector } from './ButtplugBrowserWebsocketClientConnector';
+import { WebSocket as NodeWebSocket } from 'ws';
+import { FileReader } from 'filereader';
+
+export class ButtplugNodeWebsocketClientConnector extends ButtplugBrowserWebsocketClientConnector {
+  protected _websocketConstructor =
+    NodeWebSocket as unknown as typeof WebSocket;
+  protected _filereaderConstructor = FileReader;
+}
