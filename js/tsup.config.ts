bb962ab32f6f366e47d1e293cae8f5833a1eccc2
FurryR <awathefox@gmail.com>
2024-03-24 17:22:39 +0800

    build: Migrate node bundler to tsup & fix package.json

---
+++ js/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  name: 'buttplug-js',
+  entry: ['src/index.ts'],
+  target: ['es6'],
+  format: ['cjs', 'esm'],
+  outDir: 'dist/main',
+  platform: 'node',
+  clean: true,
+  sourcemap: true,
+  dts: true,
+});
